67faa610e1023cb35f5ca8b0d1319199e4df0b01
paulosjdr <134538834+paulosjdr@users.noreply.github.com>
2023-09-19 13:30:22 -0300

    Create beecrowd | 1010.c

---
+++ beecrowd | 1010.c
@@ -0,0 +1,17 @@
+#include <iostream>  // Inclui a biblioteca de entrada/saída para entrada e saída de dados padrão.
+using namespace std; // Usa o namespace padrão, onde estão definidas as funcionalidades padrão de C++.
+
+int main() {  // Declaração da função principal "main" que retorna um inteiro (int).
+
+    int cod1, cod2, qtd1, qtd2;  // Declaração das variáveis inteiras cod1, cod2, qtd1 e qtd2.
+    double vlr1, vlr2, total;     // Declaração das variáveis de ponto flutuante (double) vlr1, vlr2 e total.
+
+    scanf("%d %d %lf", &cod1, &qtd1, &vlr1);  // Lê três valores inteiros e de ponto flutuante da entrada padrão e os armazena nas variáveis cod1, qtd1 e vlr1.
+    scanf("%d %d %lf", &cod2, &qtd2, &vlr2);  // Lê mais três valores inteiros e de ponto flutuante da entrada padrão e os armazena nas variáveis cod2, qtd2 e vlr2.
+
+    total = (qtd1 * vlr1) + (qtd2 * vlr2);  // Calcula o total somando o produto da quantidade pelo valor para cada item.
+
+    printf("VALOR A PAGAR: R$ %.2lf\n", total);  // Imprime o valor total formatado como moeda brasileira (R$) com duas casas decimais.
+
+    return 0;  // Retorna zero para indicar que o programa foi executado com sucesso.
+}
